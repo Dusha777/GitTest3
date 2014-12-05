b22b00f19ec52a7704e236ea4bcf4aea29bae10d
Stud <Stud@WS2-9.kolledz>
2014-12-05 16:24:08 +0200

    message5

--- src/pack1/MainGittest3.java
+++ src/pack1/MainGittest3.java
@@ -5,6 +5,7 @@ public class MainGittest3 {
 	public static void main(String[] args) {
 		System.out.println("Hello Gittest3");
 		System.out.println("Hello Gittest3");
+		System.out.println("Hello Gittest5");
 
 	}
 
